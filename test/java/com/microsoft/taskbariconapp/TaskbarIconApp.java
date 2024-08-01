52f26c4d91102670bb5e56168ba1c0ac84c71a32
Derek Keeler <dekeeler@microsoft.com>
2024-08-01 14:45:19 -0700

    Initial commit

---
+++ test/java/com/microsoft/taskbariconapp/TaskbarIconApp.java
@@ -0,0 +1,13 @@
+package com.example.taskbariconapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskbarIconAppTest {
+
+    @Test
+    void testMain() {
+        assertTrue(true, "Placeholder test");
+    }
+}
